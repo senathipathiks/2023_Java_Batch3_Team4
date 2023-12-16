1e9da7ae5f2f2ccf76973095dd528f4297c757f6
Sanjaykhanna <sanjaykhanna9122001@gmail.com>
2023-12-16 14:15:33 +0530

    Updated by SanjayKhanna

--- MyClass.java
+++ MyClass.java
@@ -3,4 +3,10 @@ class MyClass {
   {
     System.out.println("Sum of two numbers: " &x+y);
   }
+  void quotientDivision(int x,int y){
+	  System.out.println("Quotient of Division of two numbers"+(x/y));
+  }
+  void remainderDivision(int x,int y){
+	  System.out.println("Remainder of Division of two numbers"+(x%y));
+  }
 }

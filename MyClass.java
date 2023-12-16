b91a5362ca3db49d65ec19cb482b0a9ea846a30d
Ranjitha Rajaraman <ranjithaclg@gmail.com>
2023-12-16 14:25:20 +0530

    Ranjitha Updated

--- MyClass.java
+++ MyClass.java
@@ -3,4 +3,11 @@ class MyClass {
   {
     System.out.println("Sum of two numbers: " &x+y);
   }
+  void subtract(int x,int y){
+	  System.out.println("Subtract of two numbers :"x-y);
+  }
+void multiply(int x,int y){
+Sytem.out.println("Multiple of two numbers :"x*y);
 }
+}
+
